d9b60154507d0debf4b18795c1ef37e87483b321
DevanshB273 <works.devansh.bhardwaj@gmail.com>
2023-09-16 15:34:15 +0530

    step2

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,44 @@
-import { useState } from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Navigationbar/Header';
-import Slider from './components/Imagerotation/Section1';
-import Content1 from './components/Imageandtext/content1';
-import Content2 from './components/Imageandtext/content2';
-import BlogGrid from './components/Blog/blog-grid';
+import "./App.css";
+import React from "react";
+import { Link, Route, Routes } from "react-router-dom";
+
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Blog from "./Pages/Blog";
+import Team from "./Pages/Team";
+import Sponsors from "./Pages/Sponsors";
+
 function App() {
-  
   return (
-    <div className="app-container">
-      <div className="top">
-         <Header />
-      </div>
-      <Slider />
-      <br />
-      <br />
-      <Content1 />
-      <br />
-      <Content2 />
-      <br />
-      <br />
-      <div className="Blog">
-         <BlogGrid />
-      </div>
+    <div className="container">
+      <nav>
+        <ul>
+          <Link to="/" class="list">
+            Home
+          </Link>
+          <Link to="/blog" class="list">
+            Blog
+          </Link>
+          <Link to="/team" class="list">
+            Team
+          </Link>
+          <Link to="/contact" class="list">
+            Contact
+          </Link>
+          <Link to="/about" class="list">
+            About
+          </Link>
+        </ul>
+      </nav>
+
+      {/* Defining routes path and rendering components as element */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/sponsors" element={<Sponsors />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Navigationbar/Header.jsx
+++ src/components/Navigationbar/Header.jsx
@@ -12,11 +12,11 @@ function Header() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#about">About</Nav.Link>
-              <Nav.Link href="#team">Team</Nav.Link>
-              <Nav.Link href="#blogs">Blogs</Nav.Link>
-              <Nav.Link href="#sponsors">Sponsors</Nav.Link>
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/about">About</Nav.Link>
+              <Nav.Link href="/team">Team</Nav.Link>
+              <Nav.Link href="/blogs">Blogs</Nav.Link>
+              <Nav.Link href="/sponsors">Sponsors</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
